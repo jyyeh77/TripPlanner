3960dcaee86bfd2f55da9dca58759a5372f55f98
jyyeh77 <jyyeh77@gmail.com>
2016-08-16 11:27:04 -0400

    Set up error handling

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ var express = require('express');
 var swig = require('swig');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var path = require('path'); // might be unnecessary
 
 var app = express();
-var wikiRouter = require('./routes/wiki');
-var usersRouter = require('./routes/users');
 
 app.engine('html', swig.renderFile);
 swig.setDefaults({ cache: false });
@@ -20,16 +19,30 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/public'));
 
-app.use('/wiki', wikiRouter);
-app.use('/users', usersRouter);
 
 app.get('/', function (req, res) {
 	res.redirect('/wiki');
 });
 
-app.use(function (err, req, res, next) {
+// app.use(function (err, req, res, next) {
+// 	console.error(err);
+// 	res.status(500).send(err.message);
+// });
+
+// catch 404 (i.e., no route was hit) and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// handle all errors (anything passed into `next()`)
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
 	console.error(err);
-	res.status(500).send(err.message);
+	res.render(
+		// ... fill in this part
+	);
 });
 
 module.exports = app;
